22d669046434c2de4c0e5ce1262931098505636b
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2023-01-18 04:28:59 +0530

    Hangman changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import random
+import stage
 
 data = ["Manchester", "Barcelona", "Riyadh", "Torino", "Madrid"]
 selectCity = random.choice(data)
@@ -8,24 +9,33 @@ print(selectCity)
 displayList = ['_'] * len(selectCity)
 print(displayList)
 
-totScore = 3
-totLen = len(selectCity)
+wordDict = stage.alphaPlace(selectCity)
+
+
+
+lives = 6
+dashes = len(selectCity)
 while True:
     makeGuess = input("Make a guess: ")
     hanger = False
+    if makeGuess in wordDict and wordDict[makeGuess] == 0:
+        print("You have already consumed the maximum occurence of this character!")
     for char in  range(len(selectCity)):
         if makeGuess == selectCity[char] and displayList[char] == "_":
             hanger = True 
             displayList[char] = makeGuess 
-            totLen -= 1
+            wordDict[makeGuess] -= 1
+            dashes -= 1
             break
+    
     if not hanger: 
-        totScore -= 1 
-    if not totScore or not totLen:
+        lives -= 1 
+        print(stage.stages[lives])
+    if not lives or not dashes:
         break
 
     print(displayList)
-if not totLen:
+if not dashes:
     print(("You won!"))
 else:
     print("You Lost")
